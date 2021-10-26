4b12ebf2df338aa2d7fb1558e63046dac644c2aa
Will Dwelle <willdwelle@frontier.com>
2021-10-26 12:14:50 -0700

    part2

--- user.py
+++ user.py
@@ -6,39 +6,29 @@ class User:
     # adding the deposit method
     def make_deposit(self, amount):
         self.account_balance += amount
+        return self
 
     def make_withdrawl(self, amount):
         self.account_balance -= amount
+        return self
 
     def display_user_balance(self):
         print(self.name , "$",self.account_balance)
+        return self
 
     def transfer(self, amount, other_user):
         self.account_balance -= amount
         other_user.account_balance += amount
         print(self.account_balance)
         print(other_user.account_balance)
+        return self
 
 Will = User("Will Dwelle", "ww@coolguy.com")
 Gianna = User("Gianna Dwelle", "GW@selfcare.org")
 Dad = User("Denzil Dwelle", "dd@blah.com")
 
-Will.make_deposit(200)
-Will.make_deposit(500)
-Will.make_deposit(5)
-Will.make_withdrawl(700)
-#Will.display_user_balance()
+Will.make_deposit(200).make_deposit(500).make_deposit(5).make_withdrawl(700).display_user_balance()
 
-Gianna.make_deposit(600)
-Gianna.make_deposit(60)
-Gianna.make_withdrawl(500)
-Gianna.make_withdrawl(5)
-#Gianna.display_user_balance()
+Gianna.make_deposit(600).make_deposit(60).make_withdrawl(500).make_withdrawl(5).display_user_balance()
 
-Dad.make_deposit(10000)
-Dad.make_withdrawl(50)
-Dad.make_withdrawl(2500)
-Dad.make_withdrawl(600)
-#Dad.display_user_balance()
-
-Dad.transfer(200, Will)
+Dad.make_deposit(10000).make_withdrawl(50).make_withdrawl(2500).make_withdrawl(600).display_user_balance().transfer(200, Will)
